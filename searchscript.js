44f6df2a0bc53a37347f5f3823f8fff4c53aa535
PamelaSohWY <pamelasohwy@gmail.com>
2021-05-31 04:30:42 +0000

    trashicon and mrticon is workin

--- searchscript.js
+++ searchscript.js
@@ -6,6 +6,8 @@
       return data.data;
   }
 
+
+
   //28 May : currently generating with each click, but should only generate once (so add into DOM content loaded )??
   document.querySelector('#dropdownMenuButton1').addEventListener('click', async function(){
         let stations = await getStation();
@@ -16,11 +18,12 @@
             document.querySelector('#xyzstation').innerHTML += `<li>${i.name}</li>`
         }
       
+      })
 //Create new layer for Mrt 
 //gobal variable
 let mrtlayer = L.layerGroup()
 
-let divElement = document.createElement('div');
+let divElement1 = document.createElement('div');
 
 //marker 
 async function getMrtCoordinates(map){  //no need map for this bec not using map 
@@ -41,17 +44,17 @@ async function getMrtCoordinates(map){  //no need map for this bec not using map
    
     iconSize:     [20, 20], // size of the icon
     shadowSize:   [50, 64], // size of the shadow
-    iconAnchor:   [c.coordinates[1], c.coordinates[0]], // point of the icon which will correspond to marker's location
+    iconAnchor:   [c.coordinates], // point of the icon which will correspond to marker's location
     shadowAnchor: [4, 62],  // the same for the shadow
    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
    })
 
-   let marker1 = L.marker(c.coordinates[1], c.coordinates[0]); //do not add in [] dont put in nested array
-   console.log(marker1); //To check if the function is called
+   let marker = L.marker(c.coordinates,{icon: mrtIcon} ); //do not add in [] dont put in nested array
+   console.log(marker); //To check if the function is called
    // add marker to layer 
-   marker1.addTo(mrtlayer);
+   marker.addTo(mrtlayer);
    //bind popup to marker
-   marker1.bindPopup('station');
+   marker.bindPopup('station');
    
  }
     // add layer to map
@@ -61,7 +64,7 @@ async function getMrtCoordinates(map){  //no need map for this bec not using map
 
    //toggle button
   document
-  .getElementById("btn btn-secondary btn-add-mrtstation")
+  .getElementById("btn-add-mrtstation")
   .addEventListener("click", function(){
     if (map.hasLayer(mrtlayer)){
       map.removeLayer(mrtlayer);
